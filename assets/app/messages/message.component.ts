af8a1d2d63db0a77e3bda435ea4ebf5e984d6bc2
Tongtong Xu <minniexu64@gmail.com>
2017-06-14 22:03:51 -0700

    angular2

---
+++ assets/app/messages/message.component.ts
@@ -0,0 +1,30 @@
+import { Message } from "./message.model"
+import { Component, Input, Output, EventEmitter } from "@angular/core"
+@Component({
+    selector: 'app-message',
+    templateUrl: './message.component.html',
+    styles: [`
+        .author {
+            display: inline-block;
+            font-style: italic;
+            font-size: 12px;
+            width: 80%;
+
+        }
+        .config {
+            display: inline-block;
+            text-align: right;
+            font-size: 12px;
+            width: 19%;
+        }
+    `]
+})
+export class MessageComponent {
+    @Input() message: Message;
+    @Output() editClicked = new EventEmitter<string>();
+
+    color = 'red'
+    onEdit() {
+        this.editClicked.emit('A new value');
+    }
+}
